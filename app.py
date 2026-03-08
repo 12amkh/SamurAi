a4abaeeee6dec13df875a9e6c50b2f7389d5ef82
Ahmad  Alkhawaldeh <kha.ahmad2003@gmail.com>
2026-03-08 03:23:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,37 @@
 import os
 import requests
 from flask import Flask, render_template, request, jsonify, send_from_directory
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
 HF_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
-
-HUGGINGFACE_API_KEY = "REDACTED"
+HUGGINGFACE_API_KEY = os.environ.get("HUGGINGFACE_API_KEY")
 
 headers = {
     "Authorization": f"Bearer {HUGGINGFACE_API_KEY}"
 }
 
-
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
 @app.route('/summarize', methods=['POST'])
 def summarize():
-    # Get the text input from the form
     text = request.form['text']
-
     data = {
         "inputs": text
     }
-
     response = requests.post(HF_URL, headers=headers, json=data)
-
     if response.status_code == 200:
-
         summary = response.json()[0]['summary_text']
         return render_template('index.html', summary=summary, original_text=text)
     else:
-        # Handle errors
         error_message = "There was an error with the Hugging Face API request."
         return render_template('index.html', error=error_message)
-
 
 @app.route('/api/data')
 def api_data():
@@ -47,12 +41,9 @@ def api_data():
     }
     return jsonify(data)
 
-
 @app.route('/<path:filename>')
 def serve_static_file(filename):
     return send_from_directory('static', filename)
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
-
